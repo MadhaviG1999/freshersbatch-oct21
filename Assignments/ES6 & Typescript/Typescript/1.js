dac5d27c6c5a717c60e0ad133ff6a23ad91cfd7e
MadhaviG1999 <93818459+MadhaviG1999@users.noreply.github.com>
2021-12-14 15:37:01 +0530

    Add files via upload

---
+++ Assignments/ES6 & Typescript/Typescript/1.js
@@ -0,0 +1,6 @@
+let empName: string = "Rohan";   
+let empDept: string = "IT";   
+let output1: string = employeeName + " works in the " + employeeDept + " department.";   
+let output2: string = `${empName} works in the ${empDept} department.`;   
+console.log(output1); 
+console.log(output2); 
---
+++ Assignments/ES6 & Typescript/Typescript/9.js
@@ -0,0 +1,6 @@
+let empTuple = ["Rohit Sharma", 25, "JavaTpoint"];  
+console.log("Items: "+empTuple);  
+console.log("Length of Tuple Items before push: "+empTuple.length);   
+empTuple.push(10001);     
+console.log("Length of Tuple Items after push: "+empTuple.length);  
+console.log("Items: "+empTuple);  
