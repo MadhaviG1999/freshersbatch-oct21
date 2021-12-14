8e33c9cce6f53a02accfebad6668ee0bcc21e124
MadhaviG1999 <93818459+MadhaviG1999@users.noreply.github.com>
2021-12-14 12:29:23 +0530

    Add files via upload

---
+++ Assignments/Spring core/Autowiring/Body.java
@@ -0,0 +1,15 @@
+package com.Heart;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Body {
+
+	public static void main(String args[]) {
+		
+	ApplicationContext context =new ClassPathXmlApplicationContext("NewFile.xml");
+	Human human=context.getBean("human",Human.class);
+	human.startPumping();
+	
+}
+}
---
+++ Assignments/Spring core/Autowiring/Heart.java
@@ -0,0 +1,10 @@
+package com.Heart;
+
+public class Heart {
+	
+	public void pump() {
+		System.out.println("Heart is Pumping");
+		System.out.println("alive !!");
+	}
+
+}
